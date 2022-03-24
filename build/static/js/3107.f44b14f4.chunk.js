1c2c9cb925bb799841938cfed9bb64d2556d9231
Jijo John <jijojohnlikeu@gmail.com>
2022-03-24 12:47:11 +0530

    fix: updated gitignore for build

---
+++ build/static/js/3107.f44b14f4.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3107.f44b14f4.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkfoodie_panda=self.webpackChunkfoodie_panda||[]).push([[3107],{3107:function(n,e,a){a.r(e),a.d(e,{g:function(){return u}});var u=a(7923).i}}]);
+//# sourceMappingURL=3107.f44b14f4.chunk.js.map
