443b5747cfc5f21ef66396db9fa2458613567080
Jijo John <jijojohnlikeu@gmail.com>
2022-04-02 00:59:53 +0530

    changed to cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,25 @@
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-import { AppContextProvider } from './store/state';
-import { auth } from './firebase';
-import { useAuthState } from "react-firebase-hooks/auth";
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
 
-/* Theme variables and required css */
-import './styles/theme/ionicReact.css';
-import './styles/theme/variables.css';
-import Login from './pages/Login';
-
-setupIonicReact();
-
-const App: React.FC = () => {
-  const [user, loading] = useAuthState(auth);
-
-  if (!user) return <Login showLoading={loading} />;
-
+function App() {
   return (
-    <AppContextProvider>
-      <IonApp>
-        <IonReactRouter>
-          <IonTabs>
-            <IonRouterOutlet>
-              <Route exact path="/tab1">
-                <Tab1 />
-              </Route>
-              <Route exact path="/tab2">
-                <Tab2 />
-              </Route>
-              <Route path="/tab3">
-                <Tab3 />
-              </Route>
-              <Redirect exact from="/" to="/tab1" />
-            </IonRouterOutlet>
-            <IonTabBar slot="bottom" className='tabContainer'>
-              <IonTabButton tab="tab1" href="/tab1">
-                <IonIcon icon={triangle} />
-                <IonLabel>Tab 1</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="tab2" href="/tab2">
-                <IonIcon icon={ellipse} />
-                <IonLabel>Tab 2</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="tab3" href="/tab3">
-                <IonIcon icon={square} />
-                <IonLabel>Tab 3</IonLabel>
-              </IonTabButton>
-            </IonTabBar>
-          </IonTabs>
-        </IonReactRouter>
-      </IonApp>
-    </AppContextProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
